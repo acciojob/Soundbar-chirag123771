a26c2860c65819939121b3e88fd33f6113cf30f2
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-02-06 00:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+  const audio = new Audio(`sounds/${soundFile}`);
+  audio.play();
+}
+
+function stopAllSounds() {
+  const allSounds = document.querySelectorAll('audio');
+  allSounds.forEach((audio) => audio.pause());
+}
